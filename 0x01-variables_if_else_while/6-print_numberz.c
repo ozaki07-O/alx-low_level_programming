ab35ac1768fabf63f6437c99c596192add0f2e99
zakaria ouffa <ouffa0973@gmail.com>
2023-03-17 18:06:03 -0700

    no char needed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - prints number to 9
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int a;
+a = 0;
+	while 
+		(a < 10)
+		{
+			putchar('0' + a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
