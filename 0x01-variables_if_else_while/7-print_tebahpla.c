0715cc3a736420a842fef31555237cdc5b90fa1f
zakaria ouffa <ouffa0973@gmail.com>
2023-03-17 17:57:54 -0700

    alphabet reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets reversed in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
