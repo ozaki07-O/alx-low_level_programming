73c60869cb828659200e05095384032ebc348531
zakaria ouffa <ouffa0973@gmail.com>
2023-03-20 13:36:18 -0700

    3 for

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - prints number to 9
+ *
+ * Return: Always 0
+ */
+int main (){
+	int l;
+	int j;
+	int i;
+
+	for ( i = 10; i <= 18 ; i++)
+	{
+		for ( j = 10; j <= 19 ; j++)
+		{
+			for ( l = 10; l <= 19 ; l++)
+			{
+				if ((l % 10) > (j % 10) && (j % 10) > (i % 10))
+				{
+					putchar((i % 10) + '0');
+					putchar((j % 10) + '0');
+					putchar((l % 10) + '0');
+					if (l != 19 || j != 18 || i != 17)
+					{
+						putchar(',');
+						putchar(' ');
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+	return 0;
+}
+
