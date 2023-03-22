b921be76978a1b09520d357175d5f1836afcb493
zakaria ouffa <ouffa0973@gmail.com>
2023-03-21 19:25:11 -0700

    check the sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number n
+ * @n: number
+ *
+ * Return: 1 or 0 or -1
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return ('/');
+	}
+	_putchar('0');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -40,4 +40,13 @@ int _islower(int c);
  */
 int _isalpha(int c);
 
+/**
+ * print_sign - prints the sign of a number n
+ * @n: number 
+ *
+ * Return: 1 or 0 or -1
+ *
+ */
+int print_sign(int n);
+
 #endif
