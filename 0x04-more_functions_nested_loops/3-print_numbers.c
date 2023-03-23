36991220386663d55e6f60aa2f2d1072097c1744
zakaria ouffa <ouffa0973@gmail.com>
2023-03-23 00:36:05 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers, from 0 to 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
