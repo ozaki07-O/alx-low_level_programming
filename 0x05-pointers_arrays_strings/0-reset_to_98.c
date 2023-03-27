f26c44b094ac3126c73157031d63c3c11f220542
zakaria ouffa <ouffa0793@gmail.com>
2023-03-27 09:40:45 +0000

    pointer update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value a pointer points to
+ * @int *n: pointer to integer
+ * 
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - updates the value a pointer points to
+ * @int *n: pointer to integer
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n);
+#endif
