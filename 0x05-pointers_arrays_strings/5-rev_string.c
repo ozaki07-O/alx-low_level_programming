b0eaa6d89ed8c6fa3f1993c6d5db97941fbef159
zakaria ouffa <ouffa0793@gmail.com>
2023-03-28 15:20:43 +0000

    esrver

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
  * rev_string - reverse a string
  * @s: string
@@ -10,12 +11,12 @@ void rev_string(char *s)
 
 	int i;
 	char t;
-	int size = sizeof(s);
+	int size = _strlen(s);
 
 	for (i = 0; i < (size / 2); i++)
 	{
 		t = s[i];
-		s[i] = s[size - i];
-		s[size - i] = t;
+		s[i] = s[size - i - 1];
+		s[size - i -1] = t;
 	}
 }
