c67b2745256935738bf6477d0998dc88ef4198ad
zakaria ouffa <ouffa0793@gmail.com>
2023-03-30 12:18:15 +0000

    line of code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,7 @@ int main(void)
   int a[5];
   int *p;
 
-  a[2] = 1024;
+  a[2] = n;
   p = &n;
   /*
    * write your line of code here...
@@ -18,6 +18,6 @@ int main(void)
    */
   *p = 98;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[n]);
+  printf("a[2] = %d\n", a[2]);
   return (0);
 }
