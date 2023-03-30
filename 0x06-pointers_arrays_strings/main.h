6b71a2d24119b72d86db8ad19624a1504a66122f
zakaria ouffa <ouffa0793@gmail.com>
2023-03-30 09:09:43 +0000

    concat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: a pointer to string dest
+ */
+char *_strcat(char *dest, char *src);
+
+#endif
