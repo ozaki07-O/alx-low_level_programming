8afe4b07669a1646dc986b21befa23c783435964
zakaria ouffa <ouffa0793@gmail.com>
2023-03-30 09:30:33 +0000

    adds n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (j = 0; dest[j] != '\0'; j++)
 	{}
-	for (i = 0;i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[j + i] = src[i];
 	}
