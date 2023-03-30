f2c7f422835bc39d9801c3ab2fbd5735c9341d4a
zakaria ouffa <ouffa0793@gmail.com>
2023-03-30 09:11:00 +0000

    concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,12 +13,10 @@ char *_strcat(char *dest, char *src)
 
 	for (j = 0; dest[j] != '\0'; j++)
 	{}
-	
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[j + i] = src[i];
 	}
 	dest[j + i] = '\0';
-	
 	return (dest);
 }
