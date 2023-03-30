ee83b45b9c7f1153202ff22a764688c930406b94
zakaria ouffa <ouffa0793@gmail.com>
2023-03-30 10:56:35 +0000

    compare character by character

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -29,4 +29,12 @@ char *_strncat(char *dest, char *src, int n);
  */
 char *_strncpy(char *dest, char *src, int n);
 
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: diffrence
+ */
+int _strcmp(char *s1, char *s2);
 #endif
