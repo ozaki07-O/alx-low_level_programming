efaf858d75c985e28b1523cd5b8c81592a8a8374
zakaria ouffa <ouffa0793@gmail.com>
2023-04-03 22:38:06 +0000

    Start the project

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _memset - fill memory with a contant byte
+ * @s: array to set
+ * @b: the comstant value
+ * @n: number of times
+ *
+ * Return: char value of s
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif
