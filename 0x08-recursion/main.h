156af3b94c9833ad23b8743b593747153d95e2a8
zakaria ouffa <ouffa0793@gmail.com>
2023-04-05 16:10:12 +0000

    start recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string by recursion
+ * @s: string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s);
+
+#endif
