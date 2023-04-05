fe2cc8b87878512d8a5977f00b2b5bd18329846a
zakaria ouffa <ouffa0793@gmail.com>
2023-04-05 17:17:35 +0000

    lenght

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -24,4 +24,12 @@ void _puts_recursion(char *s);
  */
 void _print_rev_recursion(char *s);
 
+/**
+ * _strlen_recursion - returns string length
+ * @s: string
+ *
+ * Return: string length
+ */
+int _strlen_recursion(char *s);
+
 #endif
