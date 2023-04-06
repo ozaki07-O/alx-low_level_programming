902d74ed3ffd73c14ddc5e483005311136033de8
zakaria ouffa <ouffa0793@gmail.com>
2023-04-06 16:47:24 +0000

    power of x

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -40,4 +40,12 @@ int _strlen_recursion(char *s);
  */
 int factorial(int n);
 
+/**
+ * _pow_recursion - value of x raised to the power of y
+ * @x: base
+ * @y: power
+ * Return: x
+ */
+int _pow_recursion(int x, int y);
+
 #endif
