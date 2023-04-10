0dbcd8d1fb5bc32f7e35976d2ad4747794038176
zakaria ouffa <ouffa0793@gmail.com>
2023-04-10 08:37:21 +0000

    TheName

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: command line arguments
+ *
+ * Return: name
+ */
+int main(int argc, char **argv)
+{
+	printf ("%s\n", argv[argc - 1]);
+	return (0);
+}
