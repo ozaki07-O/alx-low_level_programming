1fa937ed5378ff71f5ff5cd7893db0b3fc05ef87
zakaria ouffa <ouffa0793@gmail.com>
2023-04-12 10:04:54 +0000

    duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,9 +15,14 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+
 	while (str[c] != '\0')
 		c++;
+
 	t = malloc(sizeof(char) * c + 1);
+	if (t == NULL)
+		return (NULL);
+
 	for (i = 0; i <= c; i++)
 	{
 		t[i] = str[i];
