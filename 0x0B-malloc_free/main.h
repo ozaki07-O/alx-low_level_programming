c2116501813e001b3aa706d6954d809bce0e70b5
zakaria ouffa <ouffa0793@gmail.com>
2023-04-12 09:38:15 +0000

    header + Readme

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of memory
+ * @c: character to creat
+ *
+ * Return: ponter to array, NULL if fail
+ */
+char *create_array(unsigned int size, char c);
+
+#endif
