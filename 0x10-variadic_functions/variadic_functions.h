15c07d480eb1c4894b4d8d9306ef3c9c81f00204
zakaria ouffa <ouffa0973@gmail.com>
2023-04-20 11:21:50 -0700

    finished

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
-
+#include <stdarg.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -30,6 +30,25 @@ void print_numbers(const char *separator, const unsigned int n, ...);
  * @n: number of strings
  */
 void print_strings(const char *separator, const unsigned int n, ...);
-
+/**
+ * print_all - prints all
+ * @format: format string that says arg types
+ *
+ */
 void print_all(const char * const format, ...);
+
+int print_c(va_list a);
+int print_i(va_list a);
+int print_f(va_list a);
+int print_s(va_list a);
+/**
+ * struct op - struct for 3
+ * @c: character to check
+ * @f: function to point to
+ */
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list a);
+} printer;
 #endif
