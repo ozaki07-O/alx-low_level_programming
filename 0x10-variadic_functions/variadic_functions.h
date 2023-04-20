9df183398f346cf9d4654e1942cdff6fa1f3ed4e
zakaria ouffa <ouffa0973@gmail.com>
2023-04-20 05:38:13 -0700

    va start (project, 1)

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
+ * sum_them_all- sum of arrguments
+ * @n: number of arguments
+ * Return: Sum or 0
+ */
+int sum_them_all(const unsigned int n, ...);
+#endif
