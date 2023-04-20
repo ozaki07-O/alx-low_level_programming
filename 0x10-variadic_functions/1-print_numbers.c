b0bcaeacb48d8d0e26552d72ffd9a586998479e3
zakaria ouffa <ouffa0973@gmail.com>
2023-04-20 08:29:18 -0700

    print numbs

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers wth separator
+ * @separator: string betzeen nums
+ * @n: number of nums
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list nums;
+
+	va_start(nums, n);
+	if (separator == NULL)
+	{
+		for (i = 0; i < n; i++)
+			printf("%d", va_arg(nums, int));
+		printf("\n");
+	}
+	{
+		if (i == 0)
+				printf("%d", va_arg(nums, int));
+		for (i = 1; i < n; i++)
+		{
+			printf("%s%d", separator, va_arg(nums, int));
+		}
+		printf("\n");
+	}
+	va_end(nums);
+	return;
+	
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,10 +9,22 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+
 /**
  * sum_them_all- sum of arrguments
  * @n: number of arguments
  * Return: Sum or 0
  */
 int sum_them_all(const unsigned int n, ...);
+
+/**
+ * print_numbers - prints numbers with separator
+ * @separator: string betzeen nums
+ * @n: number of nums
+ */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
 #endif
