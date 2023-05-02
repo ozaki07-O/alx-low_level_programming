68a0b0317200f842b87352621b95a53563a8edae
zakaria ouffa <ouffa0793@gmail.com>
2023-05-02 23:37:51 +0000

    tasks 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-		if (!h->n)
-			printf("[0] (nil)\n");
-		else
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
-		s++;
+		count++;
 	}
 	return (s);
 }
