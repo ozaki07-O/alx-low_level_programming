59a2a4032b85f3ffe4418c9587ad46c23e4ee931
zakaria ouffa <ouffa0793@gmail.com>
2023-05-05 02:05:06 +0000

    start task 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * binary_to_uint -  converts a binary number
+ * @b: binary number
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b);
+#endif
