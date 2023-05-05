ec4b19a657c6769a384beca44cbbc59b44af4cc9
zakaria ouffa <ouffa0793@gmail.com>
2023-05-05 02:22:28 +0000

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,25 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
 /**
  * binary_to_uint -  converts a binary number
  * @b: binary number
  * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b);
+
+/**
+ * print_binary - print binary
+ * @n: integer
+ */
+void print_binary(unsigned long int n);
 #endif
