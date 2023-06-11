65e8d7548a95c0a6308b98200fe5a5659b8f19ea
zakaria ouffa <ouffa0793@gmail.com>
2023-06-11 10:07:33 +0000

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * NumCheck - checks if number
+ * @s: string to check
+ * Return: boolean
+ */
+int NumCheck(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int result = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (NumCheck(argv[i]))
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+}
