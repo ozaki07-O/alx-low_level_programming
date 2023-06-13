781bed01236da63abe97432771f6d929b37a7369
zakaria ouffa <ouffa0793@gmail.com>
2023-06-13 16:52:27 +0000

    copies string up

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - copies a string to allocated memory
+ * @str: string
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	char *t;
+	int c = 0;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[c] != 0)
+		c++;
+	t = malloc(sizeof(char) * c - 1);
+	if (t == NULL)
+		return (NULL);
+	for (i = 0; i < c; i++)
+		t[i] = str[i];
+	return (t);
+}
