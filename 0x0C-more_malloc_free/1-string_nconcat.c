fc7392ee3155054418e4279c7568804f8d2e3187
zakaria ouffa <ouffa0793@gmail.com>
2023-06-15 16:47:43 +0000

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,30 +19,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[c] != 0)
+	while (s1[c] != '\0')
 		c++;
-	while (s2[d] != 0)
+	while (s2[d] != '\0')
 		d++;
 	if (n >= d)
-	{
-		t = malloc(sizeof(char) * c + d + 1);
-		if (t == NULL)
-			return (NULL);
-		for (i = 0; i < c; i++)
-			t[i] = s1[i];
-		for (i = 0; i < d; i++)
-			t[i + c] = s2[i];
-		return (t);
-	}
-
-	t = malloc(sizeof(char) * c + n);
+		n = d;
+	t = malloc(sizeof(char) * c + n + 1);
 	if (t == NULL)
 		return (NULL);
 
 	for (i = 0; i < c; i++)
 		t[i] = s1[i];
+
 	for (i = 0; i < n; i++)
 		t[i + c] = s2[i];
+
+	t[i + c] = '\0';
 
 	return (t);
 }
