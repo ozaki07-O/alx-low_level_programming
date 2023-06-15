46b76efb8d44501c2f5520ae1b088ed7aa38b4aa
zakaria ouffa <ouffa0793@gmail.com>
2023-06-15 15:58:12 +0000

    malloc exit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocates memory
+ * @b: bytes
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *t;
+
+	t= malloc(b);
+	if (t == NULL)
+		exit (98);
+
+	return (t);
+}
