e727a312ecd44bea87a7e81544b488bd928e12cf
zakaria ouffa <ouffa0793@gmail.com>
2023-06-20 19:45:43 +0000

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - initialize a variable
+ * @d: struct type
+ * Return: 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d.name == NULL)
+			d.name = (nil);
+		if (d.owner == NULL)
+			d.owner = (nil);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+	return;
+}
