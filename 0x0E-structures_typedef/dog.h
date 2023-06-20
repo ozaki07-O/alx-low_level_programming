244b2ddaf100cbaa45b0d9833ed7cd22c1f16bf6
zakaria ouffa <ouffa0793@gmail.com>
2023-06-20 19:04:51 +0000

    dog plate

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
+/**
+ * struct dog - The dog IDcard
+ * @name: dog name
+ * @age: dog age year
+ * @owner: owner's name
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif
