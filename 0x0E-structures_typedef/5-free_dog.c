1866cf07e3f25de867716d191331c67ae69eed9f
zakaria ouffa <ouffa0793@gmail.com>
2023-06-21 14:55:36 +0000

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees struct
+ * @d: struct type
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
