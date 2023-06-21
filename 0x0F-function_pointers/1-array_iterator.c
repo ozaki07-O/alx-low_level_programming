91b168e80f4d17380e02177905026c4a46d665fa
zakaria ouffa <ouffa0793@gmail.com>
2023-06-21 15:16:57 +0000

    execute

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned long int i;
 
 	if (array != NULL && action != NULL)
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 			action(array[i]);
 	}
 }
