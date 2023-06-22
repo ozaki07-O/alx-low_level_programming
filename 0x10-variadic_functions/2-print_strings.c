da046c321d637094eb129fc66917eec344310581
zakaria ouffa <ouffa0793@gmail.com>
2023-06-22 20:04:25 +0000

    strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,8 +23,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 		if (string == NULL)
 			printf("(nil)");
-
-		printf("%s", string);
+		else
+			printf("%s", string);
 	}
 	va_end(strings);
 	printf("\n");
