cc331a48e37e191a58a5618e3315607b0c0a2b19
ozaki07-O <ouffa0793@gmail.com>
2023-07-01 01:50:51 +0000

    first task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,15 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] (nil)\n", 0);
 		else
 			printf("[%d]%s\n", h->len, h->str);
 
-		count++;
 		h = h->next;
+		count++;
 	}
 
 	return (count);
