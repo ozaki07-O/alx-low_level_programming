acdf9ce14c952010c6384df5972f1c97e9882652
ozaki07-O <ouffa0793@gmail.com>
2023-07-08 00:28:44 +0000

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,32 @@
  */
 void print_binary(unsigned long int n)
 {
-	while (len >= 0)
+	unsigned long int i = 1;
+
+	i <<= ((sizeof(i) * 8) - 1);
+	if (n == 1)
 	{
-		if ((n >> len) & 1)
-			_putchar('1');
+		_putchar('1');
+		return;
+	}
+	if (n == 0)
+        {
+                _putchar('0');
+                return;
+        }
+	while (i > 0)
+	{
+		if ((i & n) == 0)
+			i = i >> 1;
 		else
+			break;
+	}
+	while (i > 0)
+	{
+		if ((i & n) == 0)
 			_putchar('0');
-		len--;
+		else
+			_putchar('1');
+		i = i >> 1;
 	}
 }
