f7a30263d0e061f1ed9fa788d46a50f225559965
zakaria ouffa <ouffa0793@gmail.com>
2023-11-25 22:13:05 +0100

    advanced

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_indianness - checks the endianness
+ *
+ * Return: number of bits
+*/
+int get_endianness()
+{
+	int num = 1;
+
+	return (*(cahr *)&num);
+}
