230e236522d99927d063ea433231db3c1cb36b19
zakaria ouffa <ouffa0793@gmail.com>
2023-11-27 17:40:41 +0100

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return(0);
-
 	bytes = read(file, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(file);
