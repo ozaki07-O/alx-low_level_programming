10ea0ece10f89f877b460decc73da5cf678b9c02
zakaria ouffa <ouffa0793@gmail.com>
2023-11-27 17:39:24 +0100

    0-file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: file name
+ * @leters: number of letters to read
+ *
+ * Returns: letters number
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filname || !letters)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return(0);
+
+	bytes = read(file, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(file);
+	return (bytes);
+}
