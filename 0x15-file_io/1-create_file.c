21e38d09c95420449f084e6271ebe811dfe23157
zakaria ouffa <ouffa0793@gmail.com>
2023-12-01 17:45:41 +0100

    1Createfile

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - string length
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+
+	return (i);
+}
+
+/**
+ * create_file - creats a file
+ * @filename: file's name
+ * @text_content: string to put in the file
+ *
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (0);
+	if (len)
+		bytes = write(file, text_content, len);
+	close(file);
+	return (bytes == len ? 1 : -1);
+}
